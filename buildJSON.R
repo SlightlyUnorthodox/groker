fc8d3a587228107aaac8b4a70adcd19f2bb40830
Jess Smith <smith.jessk@gmail.com>
2015-09-08 10:38:03 -0400

    Fix syntax error in buildJSON.R

--- buildJSON.R
+++ buildJSON.R
@@ -5,7 +5,7 @@ db <- dbConnect(SQLite(), dbname = paste('~/grokit/src/Tool_DataPath/',
   'executable/datapath.sqlite', sep = ""))
 df <- dbGetQuery(db, paste('SELECT CA.jType, CA.attribute, CR.relation',
   'FROM CatalogAttributes CA',
-  'INNER JOIN CatalogRelations CR ON CA.relID=CR.relID', sep = " ")
+  'INNER JOIN CatalogRelations CR ON CA.relID=CR.relID', sep = " "))
 output <- toJSON(df)
 fileConn <- file('~/schema.json')
 writeLines(output, fileConn)
